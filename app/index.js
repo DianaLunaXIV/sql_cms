6b745913bb6f2e29c13402820e79cca027b0bdd1
Brent B <brentbaughan1@gmail.com>
2021-05-04 14:22:25 -0400

    boilerplated functions for MVP; added .vscode/ to .gitignore

--- app/index.js
+++ app/index.js
@@ -3,16 +3,35 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-  
-    // Your port; if not 3306
+    host: process.env.DB_HOST,
     port: 3306,
-  
-    // Your username
-    user: 'root',
-  
-    // Be sure to update with your own MySQL password!
-    password: '',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'sql_cms',
   });
 
+const addDepartment = () => {};
+
+const addRole = () => {};
+
+const addEmployee = () => {};
+
+const viewDepartments = () => {};
+
+const viewRoles = () => {};
+
+const viewEmployees = () => {};
+
+const updateEmployeeRole = () => {};
+
+const updateEmployeeManager = () => {};
+
+const viewManagerReports = () => {};
+
+const deleteDepartment = () => {};
+
+const deleteRole = () => {};
+
+const deleteEmployee = () => {};
+
+const viewCombinedSalaries = () => {};
